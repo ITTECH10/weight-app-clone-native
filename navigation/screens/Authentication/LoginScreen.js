6a3d53a77e47e1f58668432fa7999d2622f2b344
ITTECH10 <ittech.a1development@gmail.com>
2021-10-14 14:31:09 +0200

    login and signup screens

--- navigation/screens/Authentication/LoginScreen.js
+++ navigation/screens/Authentication/LoginScreen.js
@@ -1,18 +1,57 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { Button } from '@ui-kitten/components'
+import { StyleSheet, Image, KeyboardAvoidingView } from 'react-native'
+import { Button, Layout, Input, useTheme } from '@ui-kitten/components'
+import AdaptiveText from '../../../constants/components/AdaptiveText'
 
-const Login = ({ navigation }) => {
+// const PasswordHiddenIcon = (props) => (
+//     <Icon {...props} name="eye" pack="material-community" />
+// )
+
+// const PasswordShownIcon = (props) => (
+//     <Icon {...props} name="eye-off" pack="material-community" />
+// )
+
+const LoginScreen = ({ navigation }) => {
+    // const [passwordVisible, setPasswordVisible] = React.useState(false)
+    const theme = useTheme()
+
+    // const showPasswordHandler = () => {
+    //     setPasswordVisible(prevState => !prevState)
+    // }
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Login Screen</Text>
-            <Button onPress={() => navigation.navigate('Signup')}>
-                Signup
-            </Button>
-        </View>
+        <Layout style={{ flex: 1 }}>
+            <Layout style={{ alignItems: 'center', justifyContent: 'center' }}>
+                <Layout style={{ width: '100%', height: 240, marginTop: 80 }}>
+                    <Image resizeMode="contain" style={{ height: '100%', width: '100%' }} source={require('./../../../assets/images/scallow-logo.png')} />
+                </Layout>
+
+                <KeyboardAvoidingView behavior="padding" style={{ width: '80%', marginTop: 20, justifyContent: 'flex-end' }}>
+                    <Layout>
+                        <Input
+                            placeholder="E-mail"
+                            size="large"
+                            style={{ marginBottom: 10 }}
+                        />
+                        <Input
+                            placeholder="Password"
+                            size="large"
+                            style={{ marginBottom: 10 }}
+                            secureTextEntry
+                        />
+                        <Button size="medium" onPress={() => navigation.navigate('Signup')}>
+                            Login
+                        </Button>
+                        <AdaptiveText onPress={() => navigation.navigate('Signup')} style={{ textAlign: 'center', marginTop: 3 }} color={theme['color-primary-default']}>
+                            Don't have an account? Signup
+                        </AdaptiveText>
+                    </Layout>
+                </KeyboardAvoidingView>
+            </Layout>
+        </Layout>
     )
 }
 
-export default Login
+export default LoginScreen
 
 const styles = StyleSheet.create({})
--- navigation/screens/Authentication/SignupScreen.js
+++ navigation/screens/Authentication/SignupScreen.js
@@ -1,15 +1,79 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { Button } from '@ui-kitten/components'
+import { StyleSheet, Image, KeyboardAvoidingView } from 'react-native'
+import { Button, Layout, Input, Datepicker, Icon, useTheme } from '@ui-kitten/components'
+import AdaptiveText from '../../../constants/components/AdaptiveText'
+
+const DateIcon = (props) => (
+    <Icon {...props} name="calendar-range" pack="material-community" />
+)
+
+// const PasswordHiddenIcon = (props) => (
+//     <Icon {...props} name="eye" pack="material-community" />
+// )
+
+// const PasswordShownIcon = (props) => (
+//     <Icon {...props} name="eye-off" pack="material-community" />
+// )
 
 const SignupScreen = ({ navigation }) => {
+    // const [passwordVisible, setPasswordVisible] = React.useState(false)
+    const theme = useTheme()
+
+    // const showPasswordHandler = () => {
+    //     setPasswordVisible(prevState => !prevState)
+    // }
+
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Signup Screen</Text>
-            <Button onPress={() => navigation.navigate('Login')}>
-                Login
-            </Button>
-        </View>
+        <Layout style={{ flex: 1 }}>
+            <Layout style={{ alignItems: 'center', justifyContent: 'center' }}>
+                <Layout style={{ width: '100%', height: 240, marginTop: 80 }}>
+                    <Image resizeMode="contain" style={{ height: '100%', width: '100%' }} source={require('./../../../assets/images/scallow-logo.png')} />
+                </Layout>
+
+                <KeyboardAvoidingView behavior="padding" style={{ width: '80%', marginTop: 20, justifyContent: 'flex-end' }}>
+                    <Layout>
+                        <Input
+                            placeholder="First name"
+                            size="large"
+                            style={{ marginBottom: 10 }}
+                        />
+                        <Input
+                            placeholder="Last name"
+                            size="large"
+                            style={{ marginBottom: 10 }}
+                        />
+                        <Datepicker
+                            placeholder="Birth date"
+                            style={{ marginBottom: 10 }}
+                            accessoryRight={DateIcon}
+                        />
+                        <Input
+                            placeholder="E-mail"
+                            size="large"
+                            style={{ marginBottom: 10 }}
+                        />
+                        <Input
+                            placeholder="Password"
+                            size="large"
+                            style={{ marginBottom: 10 }}
+                            secureTextEntry
+                        />
+                        <Input
+                            placeholder="ConfirmPassword"
+                            size="large"
+                            style={{ marginBottom: 20 }}
+                            secureTextEntry
+                        />
+                        <Button size="medium" onPress={() => navigation.navigate('Login')}>
+                            Signup
+                        </Button>
+                        <AdaptiveText onPress={() => navigation.navigate('Login')} style={{ textAlign: 'center', marginTop: 3 }} color={theme['color-primary-default']}>
+                            Already have an account? Login
+                        </AdaptiveText>
+                    </Layout>
+                </KeyboardAvoidingView>
+            </Layout>
+        </Layout>
     )
 }
 
